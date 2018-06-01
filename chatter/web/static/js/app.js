5f8cae077b1b6f2a382e15b1b1bbff0b12d5c147
Josh McArthur <joshua.mcarthur@gmail.com>
2018-06-01 13:42:27 +1200

    Add working chat application using Phoenix.Presence and websockets

--- chatter/web/static/js/app.js
+++ chatter/web/static/js/app.js
@@ -11,11 +11,80 @@
 //
 // If you no longer want to use a dependency, remember
 // to also remove its path from "config.paths.watched".
-import "phoenix_html"
+import "phoenix_html";
+import { Socket, Presence } from "phoenix";
 
 // Import local files
 //
 // Local files can be imported directly using relative
 // paths "./socket" or full ones "web/static/js/socket".
 
-// import socket from "./socket"
+// import socket from "./socket";
+
+let user = document.getElementById("user").innerText;
+let socket = new Socket("/socket", { params: { user: user } });
+socket.connect();
+
+let presences = {};
+let formattedTimestamp = ts => {
+  let date = new Date(ts);
+  return date.toLocaleString();
+};
+
+let listBy = (user, { metas }) => {
+  return {
+    user,
+    onlineAt: formattedTimestamp(metas[0].online_at)
+  };
+};
+
+let userList = document.getElementById("userList");
+let render = presences => {
+  userList.innerHTML = Presence.list(presences, listBy)
+    .map(
+      presence => `
+    <li>
+      ${presence.user}
+      <br>
+      <small>online since ${presence.onlineAt}</small>
+    </li>
+  `
+    )
+    .join("");
+};
+
+let room = socket.channel("room:lobby");
+room.on("presence_state", state => {
+  presences = Presence.syncState(presences, state);
+  render(presences);
+});
+
+room.on("presence_diff", diff => {
+  presences = Presence.syncDiff(presences, diff);
+  render(presences);
+});
+
+room.join();
+
+let messageForm = document.getElementById("new-message-form");
+let messageInput = document.getElementById("newMessage");
+messageForm.addEventListener("submit", e => {
+  e.preventDefault();
+  if (messageInput.value == "") return;
+  room.push("message:new", messageInput.value);
+  e.currentTarget.reset();
+});
+
+let messageList = document.getElementById("messageList");
+let renderMessage = message => {
+  let messageElement = document.createElement("li");
+  messageElement.innerHTML = `
+    <b>${message.user}</b>
+    <i>${formattedTimestamp(message.timestamp)}</li>
+    <p>${message.body}</p>
+  `;
+
+  messageList.appendChild(messageElement);
+};
+
+room.on("message:new", renderMessage);
